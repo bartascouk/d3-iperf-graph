478c4548f3b3b5d7cdd5750d20520c37a54b34f9
Bart <bartascouk@users.noreply.github.com>
2019-06-18 11:13:01 +0100

    Update iperfJob.sh

--- multi-graph/iperfJob.sh
+++ multi-graph/iperfJob.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-TS=
-
+IP_ADDRESS=
 TS=`date +%Y-%m-%d--%H-%M-%S`
-log_file=/var/www/html/data--${TS}.json
+$LOG_FILE=/var/www/html/data--${TS}.json
 
-iperf3 -c dna-solid-uk.dnae.com -t 60 -R --json --logfile $log_file
+iperf3 -c $IP_ADDRESS -t 60 -R --json --logfile $LOG_FILE
